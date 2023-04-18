fff8a9b485f4ff00f565d46d9bd32b880c757638
0xMaxou <68082106+LazarusBot@users.noreply.github.com>
2023-04-18 22:30:17 +0200

    Add a Changing Language option Add a button to switch for square or linear views

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -9,80 +9,174 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State private var isMultiColumn: Bool = false
+    @State var ChangeLanguage: Bool = false
+    
+    private var columns: [GridItem] {
+        if isMultiColumn {
+            
+            return [
+                GridItem(.flexible()),
+                GridItem(.flexible())
+            ]
+        } else {
+            return [
+                GridItem(.flexible())
+            ]
+        }
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        
+        ZStack {
+            
+            ZStack {
+                
+                VStack {
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "line.3.horizontal.decrease")
+                                .font(.title)
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            withAnimation(.easeInOut(duration: 0.2)) { ChangeLanguage.toggle() }
+                                
+                        } label: {
+                            Rectangle()
+                                .foregroundColor(Color.gray.opacity(0.1))
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(15)
+                                .shadow(radius: 2)
+                                .overlay{
+                                    
+                                    ZStack {
+                                        Image(ChangeLanguage ? "france" : "england")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                            .cornerRadius(10)
+                                    }
+                                    
+                                }
+                        }
+                        
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .padding()
+                    
+                    Circle()
+                        .foregroundColor(Color.purple)
+                        .frame(width: 400, height: 400)
+                        .blur(radius: 90)
+                        .padding(.trailing, 240)
+                        .padding(.top, -120)
+                    
+                    HStack{
+                        Text("hello-string")
+                            .foregroundColor(.white)
+                            .font(.title3)
+                        
+                        Spacer()
                     }
+                    .padding(.top, -280)
+                    .padding(.leading, UIScreen.main.bounds.width - 370)
+                    
+                    Text("title")
+                        .font(.system(size: 58))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.leading, ChangeLanguage ? UIScreen.main.bounds.width - 410 : UIScreen.main.bounds.width - 480)
+                        .padding(.top, -260)
+                    
+                    Spacer(minLength: 40)
+                    
+                    HStack {
+                        
+                        Text("subtitle")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .fontWeight(.medium)
+                        
+                        Spacer(minLength: 0)
+                        
+                        Button {
+                            isMultiColumn.toggle()
+                        } label: {
+                            Rectangle()
+                                .foregroundColor(isMultiColumn ? Color.black.opacity(0.1) : Color.gray.opacity(0.1))
+                                .frame(width: 40, height: 40)
+                                .cornerRadius(10)
+                                .shadow(radius: 2)
+                                .overlay{
+                                    
+                                    ZStack {
+                                        Image(systemName: "square.grid.2x2.fill")
+                                            .resizable()
+                                            .frame(width: 20, height: 20)
+                                            .foregroundColor(.white)
+                                           
+                                    }
+                                    
+                                }
+                        }
+                        
+                        Button {
+                            isMultiColumn.toggle()
+                        } label: {
+                            Rectangle()
+                                .foregroundColor(isMultiColumn ? Color.gray.opacity(0.1) : Color.black.opacity(0.1))
+                                .frame(width: 40, height: 40)
+                                .cornerRadius(10)
+                                .shadow(radius: 2)
+                                .overlay{
+                                    
+                                    ZStack {
+                                        Image(systemName: "line.3.horizontal")
+                                            .resizable()
+                                            .frame(width: 20, height: 10)
+                                            .cornerRadius(0)
+                                            .foregroundColor(.white)
+                                    }
+                                    
+                                }
+                        }
+                    }
+                    .padding()
+                    .padding(.top, -140)
+                    .padding(.leading, UIScreen.main.bounds.width - 385)
+                    
+                    Spacer(minLength: -22)
+                    
+                    ScrollView {
+                        
+                        LazyVGrid(columns: columns, alignment: .trailing, spacing: 5) {
+                            
+                            ForEach(0...3, id: \.self) {item in
+                                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                    .fill(.white.opacity(0.5))
+                                    .frame(width: isMultiColumn ? 180 : 370, height: isMultiColumn ? 220 : 90)
+                            }
+                        }
+                        .padding()
+                    }
+                    .padding(.top, -70)
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                
             }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .background(Color.gray.opacity(0.2))
+        .environment(\.locale, Locale.init(identifier: ChangeLanguage ? "fr" : "en"))
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
